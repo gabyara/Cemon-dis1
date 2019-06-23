976526d09994f7ac2936f5c598c4f34db11cbdb6
gabyara <47186378+gabyara@users.noreply.github.com>
2019-06-23 00:14:14 -0400

    Update cliente.php

--- cliente.php
+++ cliente.php
@@ -8,7 +8,7 @@ $fecha = $_POST['fecha'];
 
 $ruta = 'https://cemon--dis1.herokuapp.com';
 
-$cliente = new nusoap_client($ruta."/".$componente.".php",true);
+$cliente = new nusoap_client($ruta."/".$componente.".php",false);
 
 function randomAlpha() {
    
