9fb3787bd2fd563e8cbdb001b5bc4113102e9ec9
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-06-28 13:04:37 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -11,11 +11,9 @@ $servicio->register("MiFuncion", array('componente' => 'xsd:string', 'probabilid
 
 function MiFuncion($componente, $probabilidad){
 
-	$resultadoDisponibilidad = $probabilidad;
-	$resultado = "La disponibilidad del componente " . $componente . " es de: " . $resultadoDisponibilidad;	
 	$respuesta = array(
-		'frase' => $resultado,
-		'probabilidad' => $resultadoDisponibilidad
+		'componente' => $componente,
+		'probabilidad' => $probabilidad
 	);
 
 	return json_encode($respuesta);
@@ -24,5 +22,6 @@ function MiFuncion($componente, $probabilidad){
 
 $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
 $servicio->service(file_get_contents("php://input"));
+
 
 ?>
