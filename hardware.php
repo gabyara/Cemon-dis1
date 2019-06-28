b27d74e4a3efa30c712e4fba4cad10afcee9570c
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-06-28 13:05:17 -0400

    Update hardware.php

--- hardware.php
+++ hardware.php
@@ -1,26 +1,27 @@
 <?php
-	include_once 'lib/nusoap.php';
-	$servicio = new soap_server();
 
-	$ns = "urn:miserviciowsdl";
-	$servicio->configureWSDL("MiPrimerServicioWeb",$ns);
-	$servicio->schemaTargetNamespace = $ns;
+include_once 'lib/nusoap.php';
+$servicio = new soap_server();
 
-	$servicio->register("MiFuncion", array('componente' => 'xsd:string', 'probabilidad' => 'xsd:string'), array('componente' => 'xsd:string'), $ns );
+$ns = "urn:miserviciowsdl";
+$servicio->configureWSDL("MiPrimerServicioWeb",$ns);
+$servicio->schemaTargetNamespace = $ns;
 
-	function MiFuncion($componente, $probabilidad){
+$servicio->register("MiFuncion", array('componente' => 'xsd:string', 'probabilidad' => 'xsd:string'), array('return' => 'xsd:string'), $ns );
 
-		$resultadoDisponibilidad = $probabilidad;
-		$resultado = "La disponibilidad del componente " . $componente . " es de: " . $resultadoDisponibilidad;	
-		$respuesta = array(
-			'frase' => $resultado,
-			'probabilidad' => $resultadoDisponibilidad
-		);
+function MiFuncion($componente, $probabilidad){
 
-		return json_encode($respuesta);
-	
-	}
+	$respuesta = array(
+		'componente' => $componente,
+		'probabilidad' => $probabilidad
+	);
 
-	$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
-	$servicio->service(file_get_contents("php://input"));
+	return json_encode($respuesta);
+ 
+}
+
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$servicio->service(file_get_contents("php://input"));
+
+
 ?>
