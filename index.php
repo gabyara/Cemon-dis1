9d71b4ed9fb863c7e7c86854ec8558e22f5c5636
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-07-04 22:42:57 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,9 @@
                 success: function(data) {
 					json_data = JSON.parse(data);
 					$("#"+componente).html(json_data.probabilidad)
-					var current_value = $("#current_value").val() * json_data.probabilidad;
+					if(json_data.probabilidad != "No respondio"){
+					   var current_value = $("#current_value").val() * json_data.probabilidad;
+					}
 					$("#current_value").val(current_value);
 					$("#Final").html($("#current_value").val());
                 },
