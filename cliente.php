54406335c9e248864cd0f06ffaf8272ac5dfc8e6
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-07-04 23:16:55 -0400

    Update cliente.php

--- cliente.php
+++ cliente.php
@@ -8,7 +8,7 @@
 
    $ruta = 'https://cem--dis1.herokuapp.com';
 
-   $cliente = new nusoap_client($ruta."/".$componente.".php?wsdl", true);
+   $cliente = new nusoap_client($ruta."/".$componente.".php?wsdl", false);
 
    $cliente -> setEndpoint($ruta."/".$componente.".php"); 
 
