924e40c413f7672c37b5f942bd5032e496e2c56a
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-10-14 09:43:05 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -7,213 +7,38 @@
 	<meta charset="utf-8" />
 	<link href="css/bootstrap.min.css" rel="stylesheet" />
 	<link href="css/style.css" rel="stylesheet" />
-	<title>CEMON</title>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<title>Conecciones</title>
+	<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>-->
+	<script type="text/javascript" src="./jquery/jquery.3.4.js"></script>
     <script type="text/javascript">
-        function Monitorear(componente) {
-            var mes = $("#mes").val();
-            var ano= $("#ano").val();
-            var fecha = mes+'-'+ano;
-            $.ajax({
-                type: 'POST',
-                url:  'https://cemon--dis1.herokuapp.com/cliente.php',
-                data: {"componente": componente,"fecha": fecha},
+        function Solicitar() {
+			var tipo = $("#tipo").val();
+			var cod = $("#codigo").val();
+			var rif = $("#rif").val();
+			var comprador = $("#comprador").val();
+			var fecha = $("#fecha").val();
+			var direc = $("#direc").val();
+			var productos = document.getElementById("productos").value;
+			
+          	$.ajax({
+                type: 'post',
+                url:  './cliente.php',
+                data: {"tipo":tipo,"cod":cod,"rif":rif,"comprador":comprador,"fecha":fecha,"productos":productos,"direc":direc},
                 success: function(data) {
 					json_data = JSON.parse(data);
-					$("#"+componente).html(json_data.probabilidad)
-					if(json_data.probabilidad != "No respondio"){
-					   var current_value = $("#current_value").val() * json_data.probabilidad;
-					}
-					$("#current_value").val(current_value);
-					$("#Final").html($("#current_value").val());
+					console.log(json_data);
+					$("#fechafin").html(json_data.fechaF)
+					$("#costo").html(json_data.costo)
+					$("#error").html(json_data.error)
                 },
 				error: function(data){
-					console.log("Error");
+					console.log("Error: ", data);
 				}
             });
-            return "Listo";
-        }
-	</script>
-	<script type="text/javascript">
-        function Distribuidor2(componente) {
-            var mes = $("#mes").val();
-            var ano= $("#ano").val();
-            var fecha = mes+'-'+ano;
-            $.ajax({
-                method: 'POST',
-                url:  'https://cemondist2.herokuapp.com/cemon.php',
-                data: {"name": componente,"date": fecha},
-				dataType: "json",
-                success: function(data) {
-					switch (componente) {
-						case 'Hardware':
-							var dis = data.hardware;
-							break;
-						case 'BD':
-							var dis = data.bd;
-						break;
-						case 'Aplicación':
-							var dis = data.aplicacion;
-							break;
-						case 'Enlace':
-							var dis = data.enlace;
-						break;
-						case 'Router':
-							var dis = data.router;
-						break;
-					}
-					$("#"+componente).html(dis)
-                    var current_value = $("#current_value").val() * parseFloat(dis);
-                    $("#current_value").val(current_value);
-                    $("#Final").html($("#current_value").val());
-                },
-				error: function(data){
-					console.log("Error");
-				}
-            });
-            return "Listo";
-        }
-    </script>
-		<script type="text/javascript">
-        function Tienda1(componente) {
-            var mes = $("#mes").val();
-            var ano= $("#ano").val();
-            var fecha = ano+'-'+mes;
-			switch (componente) {
-				case 'Hardware':
-					var dir = "Nodo1";
-					break;
-				case 'Base de Datos':
-					var dir = "Nodo2";
-				break;
-				case 'Aplicación':
-					var dir = "Nodo3";
-					break;
-				case 'Enlace Internet':
-					var dir = "Nodo4";
-				break;
-				case 'Router Internet':
-					var dir = "Nodo5";
-				break;
-			}
-            $.ajax({
-                method: 'POST',
-                url:  'https://tiendauno-comercio-node.herokuapp.com/'+dir,
-                data: {"name": componente,"date": fecha},
-				dataType: "json",
-                success: function(data) {
-					var dis = data.response.value;
-					switch (componente) {
-						case 'Base de Datos':
-							$("#BD").html(dis)
-						break;
-						case 'Enlace Internet':
-							$("#Enlace").html(dis)
-						break;
-						case 'Router Internet':
-							$("#Router").html(dis)
-						break;
-						default:
-							$("#"+componente).html(dis)
-						break;
-					}
-                    var current_value = $("#current_value").val() * parseFloat(dis);
-                    $("#current_value").val(current_value);
-                    $("#Final").html($("#current_value").val());
-                },
-				error: function(data){
-					console.log("prueba");
-				}
-            });
-            return "Listo";
-        }
-	</script>
-	<script type="text/javascript">
-        function Tienda2(componente) {
-            var mes = $("#mes").val();
-            var ano= $("#ano").val();
-            var fecha = mes+'-'+ano;
-            $.ajax({
-                method: 'GET',
-                url:  'https://sgda.herokuapp.com/rs/component',
-                data: {"name": componente,"date": fecha},
-				dataType: "json",
-                success: function(data) {
-					var dis = data.disponibility;
-					switch (componente) {
-						case 'Base de Datos':
-							$("#BD").html(dis)
-						break;
-						case 'Enlace Internet':
-							$("#Enlace").html(dis)
-						break;
-						case 'Router Internet':
-							$("#Router").html(dis)
-						break;
-						default:
-							$("#"+componente).html(dis)
-						break;
-					}
-                    var current_value = $("#current_value").val() * parseFloat(dis);
-                    $("#current_value").val(current_value);
-                    $("#Final").html($("#current_value").val());
-				},
-				error: function(data){
-					console.log("Error");
-				}
-            });
-            return "Listo";
-        }
-    </script>
-	<script type="text/javascript">
-        function Banco1(componente) {
-            var mes = $("#mes").val();
-            var ano= $("#ano").val();
-            var fecha = mes+'-'+ano;
-            $.ajax({
-                method: 'POST',
-                url:  'https://banco-1-tarea.herokuapp.com/status',
-                data: {"name": componente,"date": fecha},
-				dataType: "json",
-                success: function(data) {
-					var dis = data.value;
-					$("#"+componente).html(dis)
-					var current_value = $("#current_value").val() * parseFloat(dis);
-					$("#current_value").val(current_value);
-					$("#Final").html($("#current_value").val());
-				},
-				error: function(data){
-					console.log("Error");
-				}
-            });
-            return "Listo";
-        }
-	</script>
-	<script type="text/javascript">
-        function Banco2(componente) {
-            var mes = $("#mes").val();
-            var ano= $("#ano").val();
-            var fecha = mes+'-'+ano;
-            $.ajax({
-                method: 'POST',
-                url:  'https://cemon-banco-2.herokuapp.com/component/'+componente,
-                data:  JSON.stringify({'name': componente,'date': fecha}),
-				dataType: "json",
-				contentType: 'application/json',
-                success: function(data) {
-					var dis = data.disponibility;
-					$("#"+componente).html(dis)
-					var current_value = $("#current_value").val() * parseFloat(dis);
-					$("#current_value").val(current_value);
-					$("#Final").html($("#current_value").val());
-				},
-				error: function(data){
-					console.log("Error");
-				}
-            });
-            return "Listo";
-        }
-    </script>
+            return "prueba";
+		}
+	</script>	
+
 </head>
 <body>
 	<header>
@@ -221,7 +46,7 @@
 			<div class="navbar navbar-default navbar-static-top">
 				<div class="container">
 					<div class="navbar-header" style=" margin-bottom: 10px;">
-						<a class="navbar-brand" href="index.php"><p>CEMON</p></a>
+						<a class="navbar-brand"><p>Conecciones</p></a>
 					</div>
 				</div>
 			</div>
@@ -235,107 +60,38 @@
 					<div class="Contenido">
 						<center><?php echo  date("d") ." del " . date("m") . " de " . date("Y");?></center>
 						<br>
-						<p style="text-align: justify;">Seleccione el mes y año en donde desea monitorear la disponibilidad de los servicios de CEMON: <br></p>
-						<form action="index.php" method="POST">
+						<p style="text-align: justify;">Solicitudes<br></p>
+						<form action="index.php" method="post">
 							<center>
-								<select id="mes" name="mes">
+								<h3>Elija el tipo de servicio:</h3>
+								<select id="tipo" name="tipo"> <!--Tipo de servicio-->
 									<?php
-			       						for ($i=1; $i<=12; $i++) {
-			            					if ($i == date('m'))
-			              						echo '<option value="'.$i.'">'.$i.'</option>';
-			            					else
-			                					echo '<option value="'.$i.'">'.$i.'</option>';
-			       						}
+										   echo '<option value="solicitud">Solicitud</option>';
+										   echo '<option value="confirmacion">Confirmacion</option>';
 		        					?>
 								</select>
-
-								<select id="ano" name="ano">
-									<?php
-			        					for($i=date('o'); $i>=1910; $i--){
-			            					if ($i == date('o'))
-			               						echo '<option value="'.$i.'">'.$i.'</option>';
-			            					else
-			                					echo '<option value="'.$i.'">'.$i.'</option>';
-										}
-									?>
-								</select>
-
-								<select id="Entidad" name="Entidad">
-									<?php
-			               				echo '<option value="Nosotros"> Nuestros servicios </option>';
-										echo '<option value="Dis2"> Ditribuidor 2</option>';
-										echo '<option value="tien1">Tienda 1</option>';
-										echo '<option value="tien2">Tienda 2</option>';
-										echo '<option value="ban1">Banco 1</option>';
-										echo '<option value="ban2">Banco 2</option>';
-
-		        					?>
-								</select>
+								<h3>Identificador de la confirmacion (solo si el servicio es de confirmacion, sino dejelo en blanco)</h3>
+								<input type="number" id="codigo" name="codigo" value=" "> <!--identificador de la solicitud para la confirmacion-->
+								<h3>RIF de la tienda</h3>
+								<input type="number" id="rif" name="rif" value="0"> <!--identificador de la tienda-->
+								<h3>Identificador del comprador de la tienda</h3>
+								<input type="number" id="comprador" name="comprador" value="0"> <!--identificador del comprador de la tienda-->
+								<h3>Fecha en que se realiza el servicio</h3>
+								<input type="date" id="fecha" name="fecha" value="<?php echo date("Y")."-".date("m")."-".date("d");?>"> <!--fecha de la solicitud-->
+								<h3>Direccion del comprador (solo si el servicio es de solicitud, sino dejelo en blanco)</h3>
+								<input  id="direc" name="direc" value="Caracas"> <!--Direccion del comprador-->
+								<h3>Array de los identificadores de los productos (solo si el servicio es de solicitud, sino dejelo en blanco)</h3>
+								<textarea id="productos" name="productos">1</textarea> <!--Array de los productos solicitados-->
+								
 							</center>
 							<br>
-								<center><input type="submit" name="monitorear" value="Monitorear"></center><br>
-								<?php
-									
-									if (isset($_POST['monitorear'])) {
-
-										switch ($_POST["Entidad"]) {
-											case "Nosotros":
-												if(is_numeric($_POST['mes']) && is_numeric($_POST['ano'])){
-													$date=$_POST['mes'] .'-'.$_POST['ano'];
-													echo '<center><strong>Fecha a consultar: '.$date.'</strong></center><br>';
-													include('vista.inc'); 
-												}else{
-													echo "Escoja una fecha";
-												}
-											break;
-											case "Dis2":
-												if(is_numeric($_POST['mes']) && is_numeric($_POST['ano'])){
-													echo '<center><strong>Distribuidor 2</strong></center><br>';
-													include('dist2.inc'); 
-												}else{
-													echo "Escoja una fecha";
-												}
-											break;
-											case "tien1":
-												if(is_numeric($_POST['mes']) && is_numeric($_POST['ano'])){
-													echo '<center><strong>Tienda 1</strong></center><br>';
-													include('tien1.inc'); 
-												}else{
-													echo "Escoja una fecha";
-												}
-											break;
-											case "tien2":
-												if(is_numeric($_POST['mes']) && is_numeric($_POST['ano'])){
-													echo '<center><strong>Tienda 2</strong></center><br>';
-													include('tien2.inc'); 
-												}else{
-													echo "Escoja una fecha";
-												}
-												break;
-											case "ban1":
-												if(is_numeric($_POST['mes']) && is_numeric($_POST['ano'])){
-													echo '<center><strong>Banco 1</strong></center><br>';
-													include('ban1.inc'); 
-												}else{
-													echo "Escoja una fecha";
-												}
-											break;
-											case "ban2":
-												if(is_numeric($_POST['mes']) && is_numeric($_POST['ano'])){
-													echo '<center><strong>Banco 2</strong></center><br>';
-													include('ban2.inc'); 
-												}else{
-													echo "Escoja una fecha";
-												}
-											break;
-											default:
-												echo "Escoja una Entidad";
-											break;
-										}
-										
-									}
-								?>
+							<center><button type="button" onclick="Solicitar()">Solicitar</button></center><br>
 						</form>
+						<p>Fecha en que se vence la solicitud</p>
+						<div id="fechafin"></div>
+						<p>Costo del envio</p>
+						<div id="costo"></div>
+						<div id="error"></div>
 					</div>
 				</div>
 			</div>
---
+++ solicitud.php
@@ -0,0 +1,42 @@
+<?php
+
+include_once 'lib/nusoap.php';
+$servicio = new soap_server();
+
+$ns = "urn:miserviciowsdl";
+$servicio->configureWSDL("MiPrimerServicioWeb",$ns);
+$servicio->schemaTargetNamespace = $ns;
+
+$servicio->register("MiFuncion", array('tipo' => 'xsd:string','rif' => 'xsd:int','comprador' => 'xsd:int','fecha' => 'xsd:date','productos' => 'xsd:string','direc' => 'xsd:string'), array('return' => 'xsd:string'), $ns );
+
+function MiFuncion($tipo,$rif,$comprador,$fecha,$productos,$direc){
+	$nuevafecha = strtotime('+30 day',strtotime($fecha));
+	$nuevafecha = date('Y-m-d' , $nuevafecha);
+	/*switch($dire){
+		case "Caracas":
+			$costo = "10";
+		break;
+	}*/
+	$respuesta = array(
+		'tipo' => $tipo,
+		'ID' => '0',  //Identificador de la solicitud
+		'rif' => $rif,
+		'comprador' => $comprador,
+		'fecha' => $fecha,
+		'fechaF' => $nuevafecha,
+		'costo' => '10',
+		'productos'=> $productos,
+		'direc' => $direc,
+		'Estatus' => "Entrante",
+		'error' => ' '
+	);
+	
+	return json_encode($respuesta);
+ 
+}
+
+$HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
+$servicio->service(file_get_contents("php://input"));
+
+
+?>
