4d1a0056c4b10cc8b365cbf51fa063b3b9ae76be
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-10-15 20:37:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 				headers: {
         			'Authorization': miToken,
     			},
-                url:  'http://bankoneapi.16mb.com/api/bill',
+                url:  'bankoneapi.16mb.com/api/bill',
                 data: {"emitter":emitter,"receiver":receiver,"bill_ref_cod":bill_ref_cod,"amount":amount,"expdate":expdate,"description":description},
                 success: function(data) {
 					//json_data = JSON.parse(data);
