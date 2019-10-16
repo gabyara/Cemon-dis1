353d5abff1516e227e0654e1f3e302ed1246187d
RicardoRAP <39196308+RicardoRAP@users.noreply.github.com>
2019-10-15 20:23:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
 	<meta charset="utf-8" />
 	<link href="css/bootstrap.min.css" rel="stylesheet" />
 	<link href="css/style.css" rel="stylesheet" />
